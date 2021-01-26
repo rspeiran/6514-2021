ff7aecbaff052b416a0c01260ed3b9db83ad9787
Rob Speiran <rspeiran@outlook.com>
2021-01-25 22:00:39 -0500

    Compressor - on - Mech in out draft.

--- Titan/src/main/java/frc/robot/commands/HippoMechIn.java
+++ Titan/src/main/java/frc/robot/commands/HippoMechIn.java
@@ -53,6 +53,7 @@ public class HippoMechIn extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        m_fuelDeliverySubSystem.HippoRetractMech();
 
     }
 
--- Titan/src/main/java/frc/robot/commands/HippoMechOut.java
+++ Titan/src/main/java/frc/robot/commands/HippoMechOut.java
@@ -53,6 +53,7 @@ public class HippoMechOut extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        m_fuelDeliverySubSystem.HippoExtendMech();
 
     }
 
